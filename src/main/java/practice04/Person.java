eb224a3871e299985c0bb2382406c5be0d33dbc2
yzeng <yzeng@thoughtworks.com>
2019-04-02 20:51:22 +0800

    [practice04] all sub-class call the basic introduce method from parents

--- src/main/java/practice04/Person.java
+++ src/main/java/practice04/Person.java
@@ -1,4 +1,27 @@
 package practice04;
 
 public class Person {
+  private String name;
+  private Integer age;
+
+  public Person(String name, Integer age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public String basicIntroduce() {
+    return "My name is " + this.name + ". I am " + this.age + " years old.";
+  }
+
+  public String introduce() {
+    return basicIntroduce();
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
 }
