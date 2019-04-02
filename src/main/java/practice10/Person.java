bfdf368041a68d22781b32502c27030fd9acc128
yzeng <yzeng@thoughtworks.com>
2019-04-02 23:17:24 +0800

    [practice10] enable teacher to own multiple classes and introduce it. Otherwise, add isTeaching methods to see if a teacher teach a particular student.

--- src/main/java/practice10/Person.java
+++ src/main/java/practice10/Person.java
@@ -1,4 +1,35 @@
 package practice10;
 
 public class Person {
+  private Integer id;
+  private String name;
+  private Integer age;
+
+  public Person(Integer id, String name, Integer age) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+  }
+
+  public String introduce() {
+    return "My name is " + this.name + ". I am " + this.age + " years old.";
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  private Integer getId() { return id; }
+
+  @Override
+  public boolean equals(Object obj) {
+    if(obj instanceof Person)
+      return id.equals(((Person)obj).getId());
+    else
+      return false;
+  }
 }
