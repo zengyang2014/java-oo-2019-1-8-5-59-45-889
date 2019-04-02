676225a1790bd9d60064fbbf895d493aa78c954e
yzeng <yzeng@thoughtworks.com>
2019-04-02 22:19:36 +0800

    [practice08] add id field for Person class. And use id to identify the person

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,26 @@
 package practice08;
 
 public class Klass {
+  private Number number;
+  private Student leader = null;
+
+  public Klass(Number klass) {
+    this.number = klass;
+  }
+
+  public Number getNumber() {
+    return number;
+  }
+
+  public String getDisplayName() {
+    return "Class " + this.number;
+  }
+
+  public void assignLeader(Student leader) {
+    this.leader = leader;
+  }
+
+  public Student getLeader() {
+    return leader;
+  }
 }
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,47 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person {
+  private static final String teacherMessage = " I am a Teacher. ";
+  private static final String ownClassMessage = " I am a Teacher. I teach ";
+  private static final String noClassMessage = " I am a Teacher. I teach No Class.";
+
+  private Klass klass;
+
+  public Teacher(Integer id, String name, Integer age) {
+    super(id, name, age);
+  }
+
+  public Teacher(Integer id, String name, Integer age, Klass klass) {
+    super(id, name, age);
+
+    this.klass = klass;
+  }
+
+  public String introduce() {
+    String introduceMessage = super.introduce();
+
+    if (klass == null) {
+      introduceMessage += noClassMessage;
+    } else {
+      introduceMessage = introduceMessage + ownClassMessage + this.klass.getDisplayName() + ".";
+    }
+
+    return introduceMessage;
+  }
+
+  public String introduceWith(Student student) {
+    String introduceMessage = super.introduce() + teacherMessage;
+
+    if(student.getKlass().getNumber().equals(this.klass.getNumber())) {
+      introduceMessage = introduceMessage + "I teach " + student.getName() + ".";
+    } else {
+      introduceMessage = introduceMessage + "I don't teach " + student.getName() + ".";
+    }
+
+    return introduceMessage;
+  }
+
+  public Klass getKlass() {
+    return this.klass;
+  }
 }
