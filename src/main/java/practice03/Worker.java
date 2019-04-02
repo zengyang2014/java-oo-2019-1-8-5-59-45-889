c48fe22538dfc33e79e4a98efdcd9915779fca56
yzeng <yzeng@thoughtworks.com>
2019-04-02 20:44:11 +0800

    [practice03] add a Worker class which extends from Person

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,13 @@
 package practice03;
 
-public class Worker {
+public class Worker extends Person{
+  private static final String message = "I am a Worker. I have a job.";
+
+  public Worker(String name, Integer age) {
+    super(name, age);
+  }
+
+  public String introduce() {
+    return message;
+  }
 }
