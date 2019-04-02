0a98eb37a3228c93003e80035e466e21ab3a7190
yzeng <yzeng@thoughtworks.com>
2019-04-02 20:40:10 +0800

    [practice02] add a Student class which extends from Person

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,20 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+  private static final String message = "I am a Student. I am at Class ";
+  private Integer klass;
+
+  public Student(String name, Integer age, Integer klass) {
+    super(name, age);
+
+    this.klass = klass;
+  }
+
+  public String introduce() {
+    return message + this.klass +".";
+  }
+
+  public Integer getKlass() {
+    return klass;
+  }
 }
