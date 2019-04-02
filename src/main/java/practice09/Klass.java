9be7d401e5eddc0740048339e2b5766d21e91ca8
yzeng <yzeng@thoughtworks.com>
2019-04-02 22:47:14 +0800

    [practice09] if the student doesn't belong to this class it cannot be assigned as a leader of this class.

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,39 @@
 package practice09;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+  private Number number;
+  private Student leader = null;
+  private List<Student> classMember;
+
+  public Klass(Number klass) {
+    this.number = klass;
+    classMember = new ArrayList<>();
+  }
+
+  public Number getNumber() {
+    return number;
+  }
+
+  public String getDisplayName() {
+    return "Class " + this.number;
+  }
+
+  void assignLeader(Student leader) {
+    if(leader.getKlass().number.equals(this.number)) {
+      this.leader = leader;
+    }
+    else
+      System.out.print("It is not one of us.\n");
+  }
+
+  public Student getLeader() {
+    return leader;
+  }
+
+  void appendMember(Student student) {
+    classMember.add(student);
+  }
 }
